2398b1e829552929b157ae7f87247cc7b1fc6bfa
Ameya Phatak <ameyaphatak88@gmail.com>
2020-10-17 23:23:13 +0530

    adding multiserver code in collegedatabase.java

--- src/collegedatabaseapp/Collegedatabase.java
+++ src/collegedatabaseapp/Collegedatabase.java
@@ -12,6 +12,12 @@ import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
+import java.io.DataInputStream;
+import java.io.PrintStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.ServerSocket;
+
 
 class Assignment implements Serializable{
 	String prob_statement;
@@ -22,12 +28,22 @@ class Assignment implements Serializable{
 
 
 
+
 //------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------
 public class Collegedatabase {
+	
+	private static ServerSocket serverSocket = null;
+	// The client socket.
+	private static Socket clientSocket = null;
+
+	// This chat server can accept up to maxClientsCount clients' connections.
+	private static final int maxClientsCount = 10;
+	private static final clientThread[] threads = new clientThread[maxClientsCount];
+
 	
 	static ArrayList<Student> student_operations(ArrayList<Student> students) throws Exception {
 		Scanner sc = new Scanner(System.in);		
@@ -442,8 +458,6 @@ public class Collegedatabase {
 //--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------	
 	
 	
-	
-	
 	static ArrayList<Teacher> teacher_operations(ArrayList<Teacher> teachers, ArrayList<Student> students) throws Exception {
 		Scanner sc = new Scanner(System.in);
 		
@@ -649,6 +663,11 @@ public class Collegedatabase {
 		return teachers;
 	}
 	
+	
+//------------------------------------------------------------------------------------------------------------------------------------------------------
+	
+	
+	
 	static Principal principal_operations(Principal p,ArrayList<Teacher> teachers, ArrayList<Student> students){
 		Scanner sc = new Scanner(System.in);
 		
@@ -725,10 +744,12 @@ public class Collegedatabase {
 	}
 		
 	
+//---------------------------------------------------------------------------------------------------------------------------------------------
+	
 	public static void main(String[] args) throws Exception {
 		
 
-		Scanner sc = new Scanner(System.in);
+		/*Scanner sc = new Scanner(System.in);
 
 		ArrayList<Student> students = new ArrayList<Student>();
 		ArrayList<Teacher> teachers = new ArrayList<Teacher>();
@@ -763,10 +784,189 @@ public class Collegedatabase {
 		//Principal p = new Principal();
 		//p = principal_operations(p, teachers, students);
 		
-		System.out.println("Thank you for using the college management app !!");
+		System.out.println("Thank you for using the college management app !!");*/
 		
-		
+		// The server socket.
+		  
+
+		  
+
+		    // The default port number.
+		    int portNumber = 2222;
+		    if (args.length < 1) {
+		      System.out.println("Usage: java MultiThreadChatServerSync <portNumber>\n"
+		          + "Now using port number=" + portNumber);
+		    } else {
+		      portNumber = Integer.valueOf(args[0]).intValue();
+		    }
+
+		    /*
+		     * Open a server socket on the portNumber (default 2222). 
+		     */
+		    try {
+		      serverSocket = new ServerSocket(portNumber);
+		    } catch (IOException e) {
+		      System.out.println(e);
+		    }
+
+		    /*
+		     * Create a client socket for each connection and pass it to a new client
+		     * thread.
+		     */
+		    while (true) {
+		      try {
+		        clientSocket = serverSocket.accept();
+		        int i = 0;
+		        for (i = 0; i < maxClientsCount; i++) {
+		          if (threads[i] == null) {
+		            (threads[i] = new clientThread(clientSocket, threads)).start();
+		            break;
+		          }
+		        }
+		        if (i == maxClientsCount) {
+		          PrintStream os = new PrintStream(clientSocket.getOutputStream());
+		          os.println("Server too busy. Try later.");
+		          os.close();
+		          clientSocket.close();
+		        }
+		      } catch (IOException e) {
+		        System.out.println(e);
+		      }
+		    }
+		  
 		
 	}
 }
+
+
+class clientThread extends Thread {
+
+	  private String clientName = null;
+	  private DataInputStream is = null;
+	  private PrintStream os = null;
+	  private Socket clientSocket = null;
+	  private final clientThread[] threads;
+	  private int maxClientsCount;
+
+	  public clientThread(Socket clientSocket, clientThread[] threads) {
+	    this.clientSocket = clientSocket;
+	    this.threads = threads;
+	    maxClientsCount = threads.length;
+	  }
+
+	  public void run() {
+	    int maxClientsCount = this.maxClientsCount;
+	    clientThread[] threads = this.threads;
+
+	    try {
+	      /*
+	       * Create input and output streams for this client.
+	       */
+	      is = new DataInputStream(clientSocket.getInputStream());
+	      os = new PrintStream(clientSocket.getOutputStream());
+	      String name;
+	      while (true) {
+	        os.println("Enter your name.");
+	        name = is.readLine().trim();
+	        if (name.indexOf('@') == -1) {
+	          break;
+	        } else {
+	          os.println("The name should not contain '@' character.");
+	        }
+	      }
+
+	      /* Welcome the new the client. */
+	      os.println("Welcome " + name
+	          + " to our chat room.\nTo leave enter /quit in a new line.");
+	      synchronized (this) {
+	        for (int i = 0; i < maxClientsCount; i++) {
+	          if (threads[i] != null && threads[i] == this) {
+	            clientName = "@" + name;
+	            break;
+	          }
+	        }
+	        for (int i = 0; i < maxClientsCount; i++) {
+	          if (threads[i] != null && threads[i] != this) {
+	            threads[i].os.println("*** A new user " + name
+	                + " entered the chat room !!! ***");
+	            System.out.println("*** A new user " + name
+	                + " entered the chat room !!! ***");
+	          }
+	        }
+	      }
+	      /* Start the conversation. */
+	      while (true) {
+	        String line = is.readLine();
+	        if (line.startsWith("/quit")) {
+	          break;
+	        }
+	        /* If the message is private sent it to the given client. */
+	        if (line.startsWith("@")) {
+	          String[] words = line.split("\\s", 2);
+	          if (words.length > 1 && words[1] != null) {
+	            words[1] = words[1].trim();
+	            if (!words[1].isEmpty()) {
+	              synchronized (this) {
+	                for (int i = 0; i < maxClientsCount; i++) {
+	                  if (threads[i] != null && threads[i] != this
+	                      && threads[i].clientName != null
+	                      && threads[i].clientName.equals(words[0])) {
+	                    threads[i].os.println("<" + name + "> " + words[1]);
+	                    /*
+	                     * Echo this message to let the client know the private
+	                     * message was sent.
+	                     */
+	                    this.os.println(">" + name + "> " + words[1]);
+	                    break;
+	                  }
+	                }
+	              }
+	            }
+	          }
+	        } else {
+	          /* The message is public, broadcast it to all other clients. */
+	          synchronized (this) {
+	            for (int i = 0; i < maxClientsCount; i++) {
+	              if (threads[i] != null && threads[i].clientName != null) {
+	                threads[i].os.println("<" + name + "> " + line);
+	              }
+	            }
+	            System.out.println("<" + name + "> " + line);
+	          }
+	        }
+	      }
+	      synchronized (this) {
+	        for (int i = 0; i < maxClientsCount; i++) {
+	          if (threads[i] != null && threads[i] != this
+	              && threads[i].clientName != null) {
+	            threads[i].os.println("*** The user " + name
+	                + " is leaving the chat room !!! ***");
+	            System.out.println("*** The user " + name
+	                    + " is leaving the chat room !!! ***");
+	          }
+	        }
+	      }
+	      os.println("*** Bye " + name + " ***");
+
+	      /*
+	       * Clean up. Set the current thread variable to null so that a new client
+	       * could be accepted by the server.
+	       */
+	      synchronized (this) {
+	        for (int i = 0; i < maxClientsCount; i++) {
+	          if (threads[i] == this) {
+	            threads[i] = null;
+	          }
+	        }
+	      }
+	      /*
+	       * Close the output stream, close the input stream, close the socket.
+	       */
+	      is.close();
+	      os.close();
+	      clientSocket.close();
+	    } catch (IOException e) {
+	    }
+	  }
+	}
 
